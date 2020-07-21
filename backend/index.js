1237c7a01d2452fb59e84d89199954809d675ab9
Cloud User <centos@ip-172-31-34-202.eu-west-1.compute.internal>
2020-07-21 20:33:03 +0000

    Added docker files

--- backend/index.js
+++ backend/index.js
@@ -26,6 +26,6 @@ app.get("/applestore", (req, res, next) => {
     });
 });
 
-app.listen(3000, () => {
- console.log("Server running on port 3000");
+app.listen(8080, () => {
+ console.log("Server running on port 8080");
 });
