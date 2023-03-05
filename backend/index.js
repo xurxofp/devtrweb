4fe7995c4f74cd3f1a5560dad2d0f52ba958aa9e
root <root@ip-172-26-8-172.eu-west-3.compute.internal>
2023-03-05 23:18:39 +0000

    Solved cors problem

--- backend/index.js
+++ backend/index.js
@@ -2,45 +2,30 @@ var express = require("express");
 var app = express();
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://devtr.es"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-});
+  });
 
 app.get("/playstore", (req, res, next) => {
-    // try {
-
     var gplay = require('google-play-scraper');
 
     // gplay.developer({devId: "DxCo Games"}).then(console.log);
-    gplay.developer({ devId: "Development Colors" }).then(result => {
+    gplay.developer({devId: "Development Colors"}).then(result => { 
         res.send(result);
     });
-
-    // } catch (error) {
-    //     console.error(error);
-    //     // Expected output: ReferenceError: nonExistentFunction is not defined
-    //     // (Note: the exact output may be browser-dependent)
-    // }
 });
 
-app.get("/applestore", (req, res, next) => {
-    // try {
-
+app.get("/applestore2", (req, res, next) => {
     var store = require('app-store-scraper');
 
     // store.developer({devId: 284882218}).then(console.log).catch(console.log);
 
-    store.developer({ devId: 1499213248 }).then(result => {
+    store.developer({devId: 1499213248}).then(result => { 
         res.send(result);
     });
-    // } catch (error) {
-    //     console.error(error);
-    //     // Expected output: ReferenceError: nonExistentFunction is not defined
-    //     // (Note: the exact output may be browser-dependent)
-    // }
 });
 
 app.listen(8080, () => {
-    console.log("Server running on port 8080");
+ console.log("Server running on port 8080");
 });
