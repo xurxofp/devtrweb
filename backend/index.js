963ec5778a21e472ef2b16161b58609a9af1590b
xurxofp <fepreitas@gmail.com>
2023-03-05 19:55:36 +0100

    .

--- backend/index.js
+++ backend/index.js
@@ -2,43 +2,43 @@ var express = require("express");
 var app = express();
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://devtr.es"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
 app.get("/playstore", (req, res, next) => {
-    try {
+    // try {
 
-        var gplay = require('google-play-scraper');
+    var gplay = require('google-play-scraper');
 
-        // gplay.developer({devId: "DxCo Games"}).then(console.log);
-        gplay.developer({ devId: "Development Colors" }).then(result => {
-            res.send(result);
-        });
+    // gplay.developer({devId: "DxCo Games"}).then(console.log);
+    gplay.developer({ devId: "Development Colors" }).then(result => {
+        res.send(result);
+    });
 
-    } catch (error) {
-        console.error(error);
-        // Expected output: ReferenceError: nonExistentFunction is not defined
-        // (Note: the exact output may be browser-dependent)
-    }
+    // } catch (error) {
+    //     console.error(error);
+    //     // Expected output: ReferenceError: nonExistentFunction is not defined
+    //     // (Note: the exact output may be browser-dependent)
+    // }
 });
 
 app.get("/applestore", (req, res, next) => {
-    try {
+    // try {
 
-        var store = require('app-store-scraper');
+    var store = require('app-store-scraper');
 
-        // store.developer({devId: 284882218}).then(console.log).catch(console.log);
+    // store.developer({devId: 284882218}).then(console.log).catch(console.log);
 
-        store.developer({ devId: 1499213248 }).then(result => {
-            res.send(result);
-        });
-    } catch (error) {
-        console.error(error);
-        // Expected output: ReferenceError: nonExistentFunction is not defined
-        // (Note: the exact output may be browser-dependent)
-    }
+    store.developer({ devId: 1499213248 }).then(result => {
+        res.send(result);
+    });
+    // } catch (error) {
+    //     console.error(error);
+    //     // Expected output: ReferenceError: nonExistentFunction is not defined
+    //     // (Note: the exact output may be browser-dependent)
+    // }
 });
 
 app.listen(8080, () => {
